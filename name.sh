2a1472af0a53370d504e7a82ef00afad6faa80bb
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2020-01-11 14:07:42 +0000

    Trying a bit of different things

---
+++ name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function byee {
+    echo "Hello $name"
+    name=ersen
+    echo "Bye $name"
+}
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source name.sh
+
+
+
+name=ecem
+echo "$name Hello"
+byee
+echo "$name byee"
+source funky.sh
