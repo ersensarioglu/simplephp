45fc8943c18a51a1be2e33f58c8e53c4ab36dd6f
Ersen Sarioglu <ersen.sarioglu@gmail.com>
2020-08-23 23:10:12 +0100

    more changes

--- funky.php
+++ funky.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "hello from php";
+$name="Description";
+$value[1][1]="ha ha hay";
+$value[2][1]="wa wa way";
+$value[2][2]="ta ta tay";
+$array[$name]=$value;
+
+//print_r($value);
+echo json_encode($array);
